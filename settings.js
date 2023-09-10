877ffc227272f6a8e522c7b4ff9ae076112ba288
XyzenIzumi <128985893+XyzenIzumi@users.noreply.github.com>
2023-09-10 10:22:11 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Kiiofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://kiicode21:<password>@kiicode.num4z0d.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "xyzen419@gmail.com" //email
+global.email_password = "zqmiqdoyzvwvppwo" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
